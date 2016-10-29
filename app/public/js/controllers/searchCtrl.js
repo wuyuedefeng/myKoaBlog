622a0d3aead62eacd287a2ed6ac37e447db3f56b
五月的风 <578595193@qq.com>
2016-10-29 23:56:19 +0800

    search layout

--- app/public/js/controllers/searchCtrl.js
+++ app/public/js/controllers/searchCtrl.js
@@ -1,6 +1,11 @@
 angular.module('myApp', [[
     '../css/search.css'
 ]])
-    .controller('searchCtrl',['$scope', function($scope) {
-        $scope.test = 'search';
-    }]);
+.controller('searchCtrl',['$scope', function($scope) {
+    $scope.search = {};
+
+    $scope.search = function () {
+        var keywords = $scope.search.keywords;
+        console.log(keywords);
+    }
+}]);
