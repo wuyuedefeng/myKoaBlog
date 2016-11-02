86a8f542d78b7bca491faef24db3e6ef07c261d7
hlkjsen <578595193@qq.com>
2016-11-02 15:53:04 +0800

    add markdown 高亮

--- app/public/js/controllers/postsShowCtrl.js
+++ app/public/js/controllers/postsShowCtrl.js
@@ -1,6 +1,8 @@
 angular.module('myApp', [[
     '../css/postsShow.css',
-    '../js/lib/marked/marked.min.js'
+    '../js/lib/marked/marked.min.js',
+    '../js/lib/highlight/src/styles/darkula.css',
+    '../js/lib/highlight/highlight.pack.js'
 ]])
     .controller('postsShowCtrl',['$scope', '$stateParams', function($scope, $stateParams) {
         $scope.post = {};
@@ -18,6 +20,7 @@ angular.module('myApp', [[
                 console.log(isErr, data);
                 if (!isErr){
                     $('#markdown').html(marked(data.post.markdown));
+                    hljs.initHighlightingOnLoad();
                 }
             }
         });
