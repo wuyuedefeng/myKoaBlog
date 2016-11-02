10f54c50fe2c125c589933fdf4ded979c442ca1a
hlkjsen <578595193@qq.com>
2016-11-02 18:22:00 +0800

    .

--- app/public/js/controllers/postsShowCtrl.js
+++ app/public/js/controllers/postsShowCtrl.js
@@ -7,8 +7,8 @@ angular.module('myApp', [[
 ], 'hc.marked'])
     .config(['markedProvider', function (markedProvider) {
         markedProvider.setOptions({
-            gfm: true,
-            tables: true,
+            // gfm: true,
+            // tables: true,
             highlight: function (code, lang) {
                 if (lang) {
                     return hljs.highlight(lang, code, true).value;
