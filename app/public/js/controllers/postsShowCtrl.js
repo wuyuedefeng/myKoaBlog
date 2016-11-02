55885772d03b5a69d454f7b01f8a284f65533bbc
hlkjsen <578595193@qq.com>
2016-11-02 16:38:01 +0800

    add angular marked

--- app/public/js/controllers/postsShowCtrl.js
+++ app/public/js/controllers/postsShowCtrl.js
@@ -1,9 +1,23 @@
 angular.module('myApp', [[
     '../css/postsShow.css',
     '../js/lib/marked/marked.min.js',
+    '../js/lib/angular-marked/angular-marked.min.js',
     '../js/lib/highlight/src/styles/darkula.css',
-    '../js/lib/highlight/highlight.pack.js'
-]])
+    '../js/lib/highlight/src/highlight.js'
+], 'hc.marked'])
+    .config(['markedProvider', function (markedProvider) {
+        markedProvider.setOptions({
+            gfm: true,
+            tables: true,
+            highlight: function (code, lang) {
+                if (lang) {
+                    return hljs.highlight(lang, code, true).value;
+                } else {
+                    return hljs.highlightAuto(code).value;
+                }
+            }
+        });
+    }])
     .controller('postsShowCtrl',['$scope', '$stateParams', function($scope, $stateParams) {
         $scope.post = {};
         $scope.$watch('$viewContentLoaded', function() {
@@ -19,6 +33,7 @@ angular.module('myApp', [[
             alwaysDo: function (isErr, data) {
                 console.log(isErr, data);
                 if (!isErr){
+                    $scope.post = data.post;
                     $('#markdown').html(marked(data.post.markdown));
                 }
             }
